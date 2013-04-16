46f611d46822d2c31d01d1d4fb99b8808f5b1f64
Chang Ju Park <pcjpcj2@gmail.com>
2013-04-16 21:20:06 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripBoards::Application.config.secret_token = '015f0024a0f85d920dd9ca5e531759c8ac04032f5aea1e1ae6e8cfe81b05af291574e5212b6b899837c2d5674fcc5b3f18a7ffe4a93186215bd47519cc5cb566'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TripBoards::Application.config.session_store :cookie_store, key: '_trip_boards_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TripBoards::Application.config.session_store :active_record_store
